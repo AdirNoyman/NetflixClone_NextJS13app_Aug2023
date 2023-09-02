c67e62dbf5fb9502822175f1adbf64db22f4774d
Adir Noyman <ofahol2007@hotmail.com>
2023-09-02 14:43:57 +0300

    Added more login and signup style

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 import Input from '@/components/Input';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [variant, setVariant] = useState('');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === 'login' ? 'register' : 'login'
+    );
+  }, []);
 
   const handleChangeEmail = (event: any) => {
     event.preventDefault();
@@ -28,12 +35,14 @@ const Auth = () => {
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
           <Image src="/images/logo.png" alt="Logo" width={150} height={150} />
-          <div className="flex justify-center">
-            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-              <h2 className="text-white text-4xl mb-8 font-semibold">
-                Sign in
-              </h2>
-              <div className="flex flex-col gap-4">
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === 'login' ? 'Sign in' : 'Sign up'}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant !== 'login' && (
                 <Input
                   label="Username"
                   onChange={handleChangeUserName}
@@ -41,33 +50,42 @@ const Auth = () => {
                   type="text"
                   value={name}
                 />
-                <Input
-                  label="Email"
-                  onChange={handleChangeEmail}
-                  id="email"
-                  type="email"
-                  value={email}
-                />
-                <Input
-                  label="Password"
-                  onChange={handleChangePassword}
-                  id="password"
-                  type="password"
-                  value={password}
-                />
-              </div>
+              )}
+              <Input
+                label="Email"
+                onChange={handleChangeEmail}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={handleChangePassword}
+                id="password"
+                type="password"
+                value={password}
+              />
             </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === 'login' ? 'Login' : 'Create account'}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === 'login'
+                ? 'First time using Netflix?'
+                : 'Already a member?'}{' '}
+              &nbsp;
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === 'login' ? 'Create an account' : 'Sign in'}
+              </span>
+            </p>
           </div>
-        </nav>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Auth;
-
-{
-  /* <div className="bg-black w-full h-full lg:bg-opacity-50">
-        
-</div> */
-}
